dbb66f207ab5415bcece0bfc7fad483f216cc04c
pablobravo73 <pablobravo73>
2023-01-13 20:03:47 -0600

    feat: change password form

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,12 @@ urlpatterns = [
     # login / logout urls
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # change password urls
+    path('password_change/',
+        auth_views.PasswordChangeView.as_view(), #PasswordChangeForm
+        name='password_change'),
+    path('password_change/done/',
+        auth_views.PasswordChangeDoneView.as_view(), #PasswordChangeDoneForm
+        name='password_change_done'),
     path('', views.dashboard, name='dashboard'),
 ]
