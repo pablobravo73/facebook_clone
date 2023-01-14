4f44d61ca813cbe1892f1170f1880f60e0102edc
pablobravo73 <pablobravo73>
2023-01-13 18:41:06 -0600

    feat: dashboard

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
-
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    # previous login url
+    # path('login/', views.user_login, name='login'),
+    # login / logout urls
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,6 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def dashboard(request):
+    return render(request,
+    'account/dashboard.html',
+    {'section': 'dashboard'})
+    
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
