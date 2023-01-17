75c4fceb52d0d500c4e52ba8bbe08f447b818053
pablobravo73 <pablobravo73>
2023-01-17 16:21:21 -0600

    Image Admin Model

--- images/models.py
+++ images/models.py
@@ -2,14 +2,17 @@ from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
 
+
+
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                            related_name='images_created',
-                            on_delete=models.CASCADE)
-    title = models.CharField(max_lenght=200)
-    slug = models.SlugField(max_lenght=200, blank=True)
-    url = models.URLField(max_lenght=2000)
-    image = models.ImageField(upload_to='images/%Y/%m/%d')
+                             related_name='images_created',
+                             on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,
+                            blank=True)
+    url = models.URLField(max_length=2000)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True)
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
@@ -17,15 +20,15 @@ class Image(models.Model):
                                         blank=True)
 
     class Meta:
-        index = [
-            models.Index(fields='-created')
+        indexes = [
+            models.Index(fields=['-created']),
         ]
         ordering = ['-created']
+
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
-
